e1c1c72e815b271564436162097918b3f29eec64
niabr <moskalev.sa@gmail.com>
2017-09-22 00:31:27 +0300

    completed the assignment

--- task_1/task/1_cmd_runner.c
+++ task_1/task/1_cmd_runner.c
@@ -9,3 +9,59 @@
    a) if execvp is used, string splitting into "path" and "args" is all you need.
 3. Collect exit codes via waitpid/WEXITSTATUS.
 */
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/wait.h>
+#include<string.h>
+
+int main(){
+  char* argv[100];
+  char *name;
+  char *token,**ptr;
+  char str[101];
+  char s[2]=" ";
+  
+  int error=0;
+  int status;
+  pid_t pid;
+
+  printf("$ ");
+  gets(&str);
+  while(strcmp(str,"exit")!=0){
+      ptr=argv;
+      
+      token=strtok(str,s);
+      
+      while(token!=NULL){
+      *ptr=token;
+      ptr++;
+      token=strtok(NULL,s);
+      }
+      *ptr=token;
+   
+      pid=fork();
+      if(pid==0){
+      	error=execvp(*argv,argv);
+      	if(error!=0){
+        	printf("Error! No such command.\n");
+        	exit(-1);
+      }
+    }
+
+      else if(pid==-1){
+	puts("Error! Unable to create child process.");
+	exit(1);
+      }
+      else{
+	wait(&status);
+	printf("Ret code: %d\n", WEXITSTATUS(status));
+	printf("$ ");
+      }
+      gets(&str);
+
+  }
+
+  return 0;
+}
