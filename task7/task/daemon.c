d2dfadc7d4fee30b7a063b79dd49433cd873b96d
Stepan Moskalev <moskalev.sa@phystech.edu>
2017-12-16 06:22:41 +0300

    diff

--- task7/task/daemon.c
+++ task7/task/daemon.c
@@ -8,3 +8,53 @@
 
 Обязательное условие: демон (на то он и демон) должен быть отвязан от любого терминала, устойчив к посылке сигналов, кроме явных SIGKILL и SIGSTOP, сохранять свой PID, не реагировать на нажатия комбинаций клавиш.
 */
+#include<stdio.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<string.h>
+#include<limits.h>
+#include<stdlib.h>
+
+int main(){
+    char dir[4097];
+    getwd(dir);
+    int fd=open("diff.log",O_RDWR|O_CREAT|O_TRUNC,0666);
+    pid_t pid=fork();
+    if(pid){
+	int fd2=open("dpid",O_RDWR|O_CREAT|O_TRUNC,0666);
+	write(fd2,&pid,sizeof(pid_t));
+	close(fd2);
+	close(fd);
+	exit(0);
+    }
+    else{
+	setsid();
+	chdir("/");
+	//umask(0);
+	close(0);
+	close(1);
+	close(2);
+
+	dup2(fd,1);
+	int dd=mkdir("/tmp/temp",0777);
+	pid=fork();
+	if(pid==0){
+	    execl("/bin/cp","cp","-r",dir,"/tmp/temp/dir",0);
+	}
+	wait();
+	while(1){
+	    pid=fork();
+	    lseek(fd,0,SEEK_SET);
+	    if(pid==0){
+		execl("/bin/diff","diff","-r",dir,"/tmp/temp/dir",0);
+		//execl("/bin/echo","echo","hello world");
+	    }
+	    wait();
+	    sleep(10);
+	}
+    }
+    return 0;
+}
